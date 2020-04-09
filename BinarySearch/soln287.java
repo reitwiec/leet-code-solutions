2db076f223bf86f415ebb6c4c1b92ea35ccba947
Reitwiec Shandilya <reitwiec@gmail.com>
2020-04-10 01:56:59 +0530

    Create soln287.java

---
+++ BinarySearch/soln287.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int hare = nums[0];
+        int tortoise = nums[0];
+        do{
+            tortoise= nums[tortoise];
+            hare = nums[nums[hare]];
+        }
+        while(tortoise!=hare);
+        
+        int ptr1 =nums[0];
+        int ptr2 =tortoise;
+        
+        while(ptr1!=ptr2){
+            ptr1=nums[ptr1];
+            ptr2=nums[ptr2];
+        }
+        
+        return ptr1;
+    }
+}
